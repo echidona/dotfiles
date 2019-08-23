5a3d698a567c6e787813442af89abecb468054f8
kagari <kagari@ieusernoMacBook-puro.local>
2019-08-23 11:35:59 +0900

    new pc

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-DOT_DIRCTORY="${HOME}/dotfiles"
+DOT_DIRCTORY="$HOME/dotfiles"
 
-ln -snfv $DOT_DIRCTORY/.config ${HOME}/.config
+ln -snfv $DOT_DIRCTORY/.config $HOME
+ln -snfv $DOT_DIRCTORY/.latexmk $HOME
+ln -snfv $DOT_DIRCTORY/.tmux.conf $HOME
+ln -snfv $DOT_DIRCTORY/.vimrc $HOME
+ln -snfv $DOT_DIRCTORY/tmux-pane-border /usr/local/bin
